dcd82674e99ae9aabae919c0585b68efe87f6575
Nguyễn Quốc Bảo An <baoandng07@gmail.com>
2025-11-16 11:29:00 -0500

    Push code footer into baoan brach

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,40 @@
 export default function Footer() {
   return (
     <>
-      <footer>Footer</footer>
+      <footer>
+        <div className="left">
+          <a href="/">Back2Me</a>
+        </div>
+        <div className="middle">
+          <div className="top">
+            <a href="">How it works</a>
+            <a href="">Contact</a>
+            <a href="">Blog</a>
+          </div>
+          <div className="bottom">
+            <a href="">
+              <i className="fa-brands fa-facebook"></i>
+            </a>
+            <a href="">
+              <i className="fa-brands fa-instagram"></i>
+            </a>
+          </div>
+        </div>
+        <div className="right">
+          <label htmlFor="">Sign up for Newsletter</label>
+          <br />
+          <form>
+            <input
+              type="text"
+              name=""
+              id=""
+              placeholder="Enter email"
+              required
+            />
+            <button>Submit</button>
+          </form>
+        </div>
+      </footer>
     </>
   );
 }
